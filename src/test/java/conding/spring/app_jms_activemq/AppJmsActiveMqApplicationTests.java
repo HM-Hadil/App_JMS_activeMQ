6846ac6e1c1f7ca6ebfa064117cbbe1f2ae5fe33
HM-Hadil <hadilhammami670@gmail.com>
2024-07-02 10:08:25 +0200

    initialize project

---
+++ src/test/java/conding/spring/app_jms_activemq/AppJmsActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package conding.spring.app_jms_activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJmsActiveMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
